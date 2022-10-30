bd5a411e6ca74608ddb57728be38ffbf7d57ef2a
NullSploit01 <harshaldharmik099@gmail.com>
2022-10-30 16:28:44 +0530

    Add a dialog box

--- src/screens/CalenderScreen.js
+++ src/screens/CalenderScreen.js
@@ -1,25 +1,34 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, View, Text, Button, Platform } from 'react-native';
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
-
-const vacation = {key: 'vacation', color: 'red', selectedDotColor: 'blue'};
-const massage = {key: 'massage', color: 'blue', selectedDotColor: 'blue'};
-const workout = {key: 'workout', color: 'green'};
+import React, { useState } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { Calendar, CalendarList, Agenda } from "react-native-calendars";
+import { Button, Dialog, CheckBox, ListItem, Avatar } from "@rneui/themed";
 
 const CalendarScreen = () => {
+  const [dialogVisible, setDialogVisible] = useState(false);
+  const [selectedDate, setDate] = useState(null);
+  const toggleDialogVisibility = (date = null) => {
+    setDialogVisible(!dialogVisible);
+    console.log(date);
+    setDate(date);
+  };
+
   return (
     <View style={styles.container}>
-      <Calendar onDayPress={day => {
-        console.log(day)
-      }} 
-      />
+      <Calendar onDayPress={(date) => toggleDialogVisibility(date)} />
+      <Dialog
+        isVisible={dialogVisible}
+        onBackdropPress={toggleDialogVisibility}
+      >
+        <Dialog.Title title="Date" />
+        <Text>{selectedDate ? selectedDate.dateString : null}</Text>
+      </Dialog>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { paddingTop: 10},
-  heading: {fontSize: 38, textAlign: 'center', padding: 10}
+  container: { paddingTop: 10 },
+  heading: { fontSize: 38, textAlign: "center", padding: 10 },
 });
 
 export default CalendarScreen;
