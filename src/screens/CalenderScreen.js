1fa6b1271b36045e52ca8b0c4dba0a4d4f960689
NullSploit01 <harshaldharmik099@gmail.com>
2022-10-30 15:53:11 +0530

    Add Calender

--- src/screens/CalenderScreen.js
+++ src/screens/CalenderScreen.js
@@ -1,15 +1,25 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useEffect } from 'react';
+import { StyleSheet, View, Text, Button, Platform } from 'react-native';
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+
+const vacation = {key: 'vacation', color: 'red', selectedDotColor: 'blue'};
+const massage = {key: 'massage', color: 'blue', selectedDotColor: 'blue'};
+const workout = {key: 'workout', color: 'green'};
+
 const CalendarScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Calendar Screen</Text>
+      <Calendar onDayPress={day => {
+        console.log(day)
+      }} 
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: "center", justifyContent: "center" },
+  container: { paddingTop: 10},
+  heading: {fontSize: 38, textAlign: 'center', padding: 10}
 });
 
 export default CalendarScreen;
